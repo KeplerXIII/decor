355617580d0cdf2e4120a9ff55b3b5093b86bfdd
a.pushkin <a.v.pushkin@gmail.com>
2022-11-08 10:29:05 +0300

    Upload

---
+++ task1.py
@@ -0,0 +1,42 @@
+import os
+import datetime
+
+def logger(old_function):
+    def new_function(*args, **kwargs):
+        result = old_function(*args, **kwargs)
+        with open('main.log', 'a', encoding='UTF-8') as f:
+            f.write(f'Время вызова: {str(datetime.datetime.now())} Наименование функции: {old_function.__name__} '
+                    f'Аргументы: {args}{kwargs} Результат: {result}\n')
+        return result
+    return new_function
+
+
+def test_1():
+
+    path = 'main.log'
+    if os.path.exists(path):
+        os.remove(path)
+
+    @logger
+    def summator(a, b=0):
+        return a + b
+
+    result = summator(2, 2)
+    assert isinstance(result, int), 'Должно вернуться целое число'
+    assert result == 4, '2 + 2 = 4'
+
+    assert os.path.exists(path), 'файл main.log должен существовать'
+
+    summator(4.3, b=2.2)
+    summator(a=0, b=0)
+
+    with open(path) as log_file:
+        log_file_content = log_file.read()
+
+    assert 'summator' in log_file_content, 'должно записаться имя функции'
+    for item in (4.3, 2.2, 6.5):
+        assert str(item) in log_file_content, f'{item} должен быть записан в файл'
+
+
+if __name__ == '__main__':
+    test_1()
--- task2.py
+++ task2.py
@@ -1,15 +1,18 @@
 import os
 import datetime
 
+
 def logger(path):
     def _logger(old_function):
         def new_function(*args, **kwargs):
             result = old_function(*args, **kwargs)
             with open(path, 'a', encoding='UTF-8') as f:
-                f.write(f'Время вызова: {str(datetime.datetime.now())}/Наименование функции: {old_function.__name__}/ '
-                        f'Аргументы: {args}{kwargs}/ Результат: {result}\n')
+                f.write(f'Время вызова: {str(datetime.datetime.now())} Наименование функции: {old_function.__name__} '
+                        f'Аргументы: {args}{kwargs} Результат: {result}\n')
             return result
+
         return new_function
+
     return _logger
 
 
@@ -18,12 +21,15 @@ def test_2():
     for path in paths:
         if os.path.exists(path):
             os.remove(path)
+
         @logger(path)
         def summator(a, b=0):
             return a + b
+
         @logger(path)
         def div(a, b):
             return a / b
+
         result = summator(2, 2)
         assert isinstance(result, int), 'Должно вернуться целое число'
         assert result == 4, '2 + 2 = 4'
@@ -36,5 +42,7 @@ def test_2():
         assert 'summator' in log_file_content, 'должно записаться имя функции'
         for item in (4.3, 2.2, 6.5):
             assert str(item) in log_file_content, f'{item} должен быть записан в файл'
+
+
 if __name__ == '__main__':
     test_2()
